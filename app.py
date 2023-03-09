68238bf66db7fe853b7427c5bd1b7dda83252df5
Paul L. Brown <paullbrown@gmail.com>
2023-03-09 09:28:49 -0600

    updated

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ def precipitation():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
-
     session.close()
     
     precip = {date: prcp for date, prcp in precipitation}
@@ -55,7 +54,6 @@ def stations():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     stations = session.query(Measurement.date, Measurement.station)\
         .filter(Measurement.date >= prev_year).all()
-    
     session.close()
 
     stationq = {date: station for date, station in stations}
@@ -69,7 +67,6 @@ def tobs():
 
     temperature = session.query(Measurement.date, Measurement.tobs).\
         filter(Measurement.date >= prev_year).all()
-    
     session.close()
 
     tempq = {date: tobs for date, tobs in temperature}
@@ -78,29 +75,45 @@ def tobs():
 
 @app.route("/api/v1.0/<start>")
 def temp_start(start):
-    start_temperature = session.query(Measurement.date, Measurement.tobs)\
-        .filter(Measurement.date >= start).all()
-    
+    start_temperature = session.query(func.min(Measurement.tobs), 
+                                      func.max(Measurement.tobs), 
+                                      func.avg(Measurement.tobs))\
+        .filter(Measurement.date >= start)\
+        .group_by(Measurement.tobs).all()
     session.close()
 
-    start_tempq = {date: tobs for date, tobs in start_temperature}
-    return jsonify(start_tempq)
+    all_temp_start = []
+    for minimum, maximum, average in start_temperature:
+        all_temp_start_dict = {}
+        all_temp_start_dict["minimum"] = minimum
+        all_temp_start_dict["maximum"] = maximum
+        all_temp_start_dict["average"] = average
+        all_temp_start.append(all_temp_start_dict)
+
+    return jsonify(all_temp_start)
 
 
 
 @app.route("/api/v1.0/<start>/<end>")
 def temp_start_end(start, end):
-    temperature_se = session.query(Measurement.date, Measurement.tobs)\
+    temperature_se = session.query(func.min(Measurement.tobs), 
+                                   func.max(Measurement.tobs), 
+                                   func.avg(Measurement.tobs))\
         .filter(Measurement.date >= start)\
-        .filter(Measurement.date < end).all()
-        
-
+        .filter(Measurement.date < end)\
+        .group_by(Measurement.date).all()
     session.close()
 
-    se_tempq = {date: tobs for date, tobs in temperature_se}
-    return jsonify(se_tempq)
+    all_temp_start_end = []
+    for minimum, maximum, average in temperature_se:
+        all_temp_start_end_dict = {}
+        all_temp_start_end_dict["minimum"] = minimum
+        all_temp_start_end_dict["maximum"] = maximum
+        all_temp_start_end_dict["average"] = average
+        all_temp_start_end.append(all_temp_start_end_dict)
+
+    return jsonify(all_temp_start_end)
 
 
-# don't forget...
 if __name__ == "__main__":
     app.run(debug=True)
